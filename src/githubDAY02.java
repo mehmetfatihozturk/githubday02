531a90b445100828f9f13585ebf632e4a468b11f
mehmetfatihozturk <140940193+mehmetfatihozturk@users.noreply.github.com>
2023-08-05 23:53:37 +0300

    first commit

--- src/githubDAY02.java
+++ src/githubDAY02.java
@@ -1,5 +1,6 @@
 public class githubDAY02 {
     public static void main(String[] args) {
         System.out.println("merhaba tekrardan gitub");
+        System.out.println("ikinci satır ekleme işlemigit ");
     }
 }
---
+++ src/githubday02_2.java
@@ -0,0 +1,6 @@
+public class githubday02_2 {
+    public static void main(String[] args) {
+        System.out.println("merhaba github");
+        System.out.println("2.dosya 1.satırgit");
+    }
+}
